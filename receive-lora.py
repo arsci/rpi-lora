291c5ab94f5f7e71aba92e511f243f33417fccc2
arsci <russerya@gmail.com>
2019-11-04 13:37:40 -0800

    lower tx power

--- receive-lora.py
+++ receive-lora.py
@@ -9,7 +9,7 @@ CS = DigitalInOut(board.CE1)
 RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-rfm9x.tx_power = 23
+rfm9x.tx_power = 13
 prev_packet = None
  
 while True:

db78e660cf4e5685ce46b17068e0d1156084e778
arsci <russerya@gmail.com>
2019-11-04 18:07:50 -0800

    rec improvements

--- scripts/receive-lora.py
+++ scripts/receive-lora.py
@@ -6,14 +6,18 @@ from digitalio import DigitalInOut, Direction, Pull
 import board
 import adafruit_ssd1306
 import adafruit_rfm9x
- 
+
+print('Setting up LoRa Receiver...')
+
 CS = DigitalInOut(board.CE1)
 RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
 rfm9x.tx_power = 13
 prev_packet = None
- 
+
+print('Listening on 915MHz for packets...')
+
 while True:
     packet = None
     try:

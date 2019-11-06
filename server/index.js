8610ac1f687a3580c18d2a41c43b33c158402c8e
arsci <russerya@gmail.com>
2019-11-06 09:32:13 -0800

    add body parser

--- server/index.js
+++ server/index.js
@@ -7,7 +7,43 @@ const app = express()
 const port = 3000
 var cors = require('cors')
 
+const bodyParser = require('body-parser');
+app.use(bodyParser);
+
+app.post('/', (req, res) => {
+
+    try {
+        resp = child.kill()
+        console.log(resp)
+        console.log('killed process')
+    }
+    catch {
+        console.log("none");
+    }
+    child = spawn('python3', ['scripts/send-single-packet-lora.py', req.body], [], function(err, stdout, stderr) { 
+        if(err) { console.log(err) }
+        if(stderr) { console.log(err) }
+        console.log(stdout); 
+    });   
+
+    child.stdout.on('data',function(chunk){
+
+        var textChunk = chunk.toString('utf8');// buffer to string
+
+        console.log(textChunk);
+    })
+
+    res.send('Sending Packet \n')
+
+})
+
 app.get('/', (req, res) => {
+
+    res.send('Server is UP \n')
+
+})
+
+app.get('/test', (req, res) => {
 
     try {
         resp = child.kill()
@@ -30,7 +66,7 @@ app.get('/', (req, res) => {
         console.log(textChunk);
     })
 
-    res.send('Sending Packet \n')
+    res.send('Sending Test Packet \n')
 
 })
 
